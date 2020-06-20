a905bc49a02ac042cedd9fa751aa534ad9343628
littlegao233 <gaoxinhong2004@hotmail.com>
2020-06-20 22:02:00 +0800

    setup

---
+++ X_multi-server_container/X_multi-server_container/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace X_multi_server_container
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
