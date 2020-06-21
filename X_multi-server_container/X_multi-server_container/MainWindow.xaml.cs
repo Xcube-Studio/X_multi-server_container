f29d87fe33cb6a94cdd07f3cbb829a2d40b32be7
xuan2006 <2044178204@qq.com>
2020-06-21 22:20:55 +0800

    FluentWindow

--- X_multi-server_container/X_multi-server_container/MainWindow.xaml.cs
+++ X_multi-server_container/X_multi-server_container/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SourceChord.FluentWPF;
+
 namespace X_multi_server_container
 {
     /// <summary>
@@ -23,7 +24,6 @@ namespace X_multi_server_container
         public MainWindow()
         {
             InitializeComponent();
-                SourceChord.FluentWPF.AcrylicWindow.
-    }
+        }
     }
 }
