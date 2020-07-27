9e0e8f39da1c99c7bd95c7fc7d9a9fc49e0a752f
gxh <46601807+littlegao233@users.noreply.github.com>
2020-07-27 11:26:05 +0800

    Preview

--- X_multi-server_container/X_multi-server_container/Properties/AssemblyInfo.cs
+++ X_multi-server_container/X_multi-server_container/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("X_multi-server_container")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("XcubeStudio版权所有")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("XcubeStudio")]
 [assembly: AssemblyProduct("X_multi-server_container")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Xcube Studio 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.*")]
